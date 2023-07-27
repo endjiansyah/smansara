78d87e9a4e15decab80e6d855acfa4283383c98a
endjiansyah <endji1507@gmail.com>
2023-07-27 14:16:44 +0700

    clone from https://github.com/endjiansyah/netbook-tailwindcss and change header / top bar

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+        fontFamily:{
+            inter:['Inter'],
+            sans:['Inter'],
+            
+        }
+        ,
+        container: {
+            padding:{
+                sm: '20px',
+                md: '20px'
+            },
+            screens:{
+                xl: '1170px',
+            },
+            center:true,
+        },
+        extend: {
+            colors: {
+                primary: '#217BF4',
+                title: '#0A093D',
+                secondary : '#656464',
+                blueli : '#171648',
+            }
+        }
+      },
+    plugins: [],
+  }
